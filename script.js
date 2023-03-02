49ee72f9df3b2cbf4258f45b84e779b256611a1d
Federico Marcattini <fedemarcattini96@gmail.com>
2023-03-02 15:37:24 -0300

    Proyecto Final CV

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+function animar2() {
+    document.getElementById("estudios").classList.toggle ("animacion");
+}
+
+document.getElementById("boton2").onclick = function () {
+    animar2();
+}
+
+function animar3() {
+    document.getElementById("proyectos").classList.toggle ("animacion");
+}
+
+document.getElementById("boton3").onclick = function () {
+    animar3();
+}
